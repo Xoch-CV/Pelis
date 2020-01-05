a681aaa1539ac56cb5126565b3d355556102b913
Xoch-CV <xoch.cv@gmail.com>
2020-01-05 18:39:56 -0300

    Primer commit

---
+++ app/Movie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    public $table = 'movies';
+    public $guarded = [];
+    //public $fillable = [''];
+}
